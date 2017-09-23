25bf1831aad86aa2e8ba8e0298d615a0904777d5
Štěpán Tomsa <stepantomsa@icloud.com>
2017-09-23 09:23:45 +0200

    Parametrize all the tests!
    The original one big test split into smaller ones:
    * Testing zero: hard-coded result.
    * Testing positive integers: recursion.
    * Testing other input: ValueError.

--- test_fac.py
+++ test_fac.py
@@ -1,22 +1,21 @@
 from fac import fac
-from pytest import raises
+import pytest
 
 
-def test_fac():
-    assert fac(1) == 1
-    assert fac(2) == 2
-    assert fac(3) == 6
-    assert fac(4) == 24
-    assert fac(5) == 120
-    assert fac(10) == 3628800
-
+def test_zero():
     assert fac(0) == 1
 
-    with raises(ValueError):
-        fac(-1)
 
-    with raises(ValueError):
-        fac(3.0)
+test_data = [(1, 1), (2, 2), (3, 6), (4, 24), (5, 120), (10, 3628800)]
 
-    with raises(ValueError):
-        fac('5')
+@pytest.mark.parametrize(('input', 'result'), test_data)
+def test_positive_integer(input, result):
+    assert fac(input) == result
+
+
+test_data = [-1, 3.0, '5']
+
+@pytest.mark.parametrize(('input'), test_data)
+def test_invalid(input):
+    with pytest.raises(ValueError):
+        fac(input)
